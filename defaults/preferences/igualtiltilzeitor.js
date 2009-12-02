2be57bffb9864e39336e299ba8e14974da2edb46
Gabriel Gilini <gabrielgilini@gmail.com>
2009-12-01 23:16:00 -0200

    Initial commit with automatically generated structure

---
+++ defaults/preferences/igualtiltilzeitor.js
@@ -0,0 +1,2 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.igualtiltilzeitor@gabriel.punisher.gilini.description", "chrome://igualtiltilzeitor/locale/igualtiltilzeitor.properties");
